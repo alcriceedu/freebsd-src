4941084e77773cd24e52fd55f141f18f26b53c98
Eliot H. Solomon <ehs3@rice.edu>
2021-06-25 16:35:17 -0400

    Refine pmap_protect_l3c and pmap_remove_l3c.

--- sys/arm64/arm64/pmap.c
+++ sys/arm64/arm64/pmap.c
@@ -400,7 +400,7 @@ static int pmap_remove_l2(pmap_t pmap, pt_entry_t *l2, vm_offset_t sva,
 static int pmap_remove_l3(pmap_t pmap, pt_entry_t *l3, vm_offset_t sva,
     pd_entry_t l2e, struct spglist *free, struct rwlock **lockp);
 static int pmap_remove_l3c(pmap_t pmap, pt_entry_t *start_l3, vm_offset_t sva,
-    vm_offset_t eva, vm_offset_t *vap, pd_entry_t l2e, struct spglist *free,
+    vm_offset_t eva, vm_offset_t *vap, vm_page_t l3pg, struct spglist *free,
     struct rwlock **lockp);
 static void pmap_reset_asid_set(pmap_t pmap);
 static boolean_t pmap_try_insert_pv_entry(pmap_t pmap, vm_offset_t va,
@@ -2942,12 +2942,12 @@ pmap_remove_l3(pmap_t pmap, pt_entry_t *l3, vm_offset_t va,
  */
 static int
 pmap_remove_l3c(pmap_t pmap, pt_entry_t *start_l3, vm_offset_t sva,
-    vm_offset_t eva, vm_offset_t *vap, pd_entry_t l2e, struct spglist *free,
+    vm_offset_t eva, vm_offset_t *vap, vm_page_t l3pg, struct spglist *free,
     struct rwlock **lockp)
 {
 	int unwire_ret;
 	pt_entry_t *current_l3, *end_l3, mask, nbits, old_first_l3, old_l3;
-	vm_page_t l3pg, m, mt;
+	vm_page_t m, mt;
 	struct md_page *pvh;
 	struct rwlock *new_lock;
 
@@ -3001,8 +3001,6 @@ pmap_remove_l3c(pmap_t pmap, pt_entry_t *start_l3, vm_offset_t sva,
                 }
                 m = PHYS_TO_VM_PAGE(old_first_l3 & ~ATTR_MASK);
                 pvh = page_to_pvh(m);
-                l3pg = sva < VM_MAXUSER_ADDRESS ? PHYS_TO_VM_PAGE(l2e &
-                    ~ATTR_MASK) : NULL;
                 for (mt = m; mt < &m[NCONTIGUOUS]; mt++, sva += PAGE_SIZE) {
 			if (pmap_pte_dirty(pmap, old_first_l3))
                                 vm_page_dirty(mt);
@@ -3057,7 +3055,7 @@ pmap_remove_l3_range(pmap_t pmap, pd_entry_t l2e, vm_offset_t sva,
 			if (((sva & (NCONTIGUOUS * L3_SIZE - 1)) == 0) &&
 			    (sva + NCONTIGUOUS * L3_SIZE <= eva)) {
 				if (pmap_remove_l3c(pmap, l3, sva, eva, &va,
-				    l2e, free, lockp)) {
+				    l3pg, free, lockp)) {
 					sva += NCONTIGUOUS * L3_SIZE;
 					break; /* L3 table was unmapped. */
 				} else {
@@ -3388,12 +3386,15 @@ static void
 pmap_protect_l3c(pmap_t pmap, pt_entry_t *start_l3, vm_offset_t sva,
     vm_offset_t *vap, vm_offset_t va_next, pt_entry_t mask, pt_entry_t nbits)
 {
+	bool dirty;
 	pt_entry_t l3, *l3p;
 
 	PMAP_LOCK_ASSERT(pmap, MA_OWNED);
 	PMAP_ASSERT_STAGE1(pmap);
 	KASSERT(((uintptr_t)start_l3 & (NCONTIGUOUS * sizeof(pt_entry_t) - 1))
 	    == 0, ("pmap_remove_l3c: start_l3 is not aligned"));
+
+	dirty = false;
 
 	for (l3p = start_l3; l3p < start_l3 + NCONTIGUOUS; l3p++, sva +=
 	    L3_SIZE) {
@@ -3415,20 +3416,28 @@ retry:
 			continue;
 		}
 
-		/*
-		 * When a dirty read/write mapping is write protected,
-		 * update the page's dirty field.
-		 */
-		if ((l3 & ATTR_SW_MANAGED) != 0 &&
-		    (nbits & ATTR_S1_AP(ATTR_S1_AP_RO)) != 0 &&
-		    pmap_pte_dirty(pmap, l3))
-			vm_page_dirty(PHYS_TO_VM_PAGE(l3 & ~ATTR_MASK));
+		if ((l3 & (ATTR_S1_AP_RW_BIT | ATTR_SW_DBM)) ==
+		    (ATTR_S1_AP(ATTR_S1_AP_RW) | ATTR_SW_DBM))
+			dirty = true;
 
 		if (!atomic_fcmpset_64(l3p, &l3, (l3 & ~mask) | nbits))
 			goto retry;
 		if (*vap == va_next)
 			*vap = sva;
 	}
+	/*
+	 * When a dirty read/write mapping is write protected,
+	 * update the dirty field of each page in the superpage.
+	 */
+	if ((l3 & ATTR_SW_MANAGED) != 0 &&
+	    (nbits & ATTR_S1_AP(ATTR_S1_AP_RO)) != 0 &&
+	    dirty) {
+		for (l3p = start_l3; l3p < start_l3 + NCONTIGUOUS; l3p++) {
+			l3 = pmap_load(l3p);
+			vm_page_dirty(PHYS_TO_VM_PAGE(l3 & ~ATTR_MASK));
+		}
+	}
+
 }
 
 /*
