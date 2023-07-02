e3546d0e3a55814705f0483c74f9643a06dfdd43
Alan Cox <alc@rice.edu>
2023-07-02 00:45:59 -0500

    Deindent a bit.

--- sys/arm64/arm64/pmap.c
+++ sys/arm64/arm64/pmap.c
@@ -3732,9 +3732,9 @@ pmap_remove_l3_range(pmap_t pmap, pd_entry_t l2e, vm_offset_t sva,
 					sva += L3C_SIZE - L3_SIZE;
 					continue;
 				}
-			} else {
-				pmap_demote_l3c(pmap, l3, sva);
 			}
+
+			pmap_demote_l3c(pmap, l3, sva);
 		}
 		old_l3 = pmap_load_clear(l3);
 		if ((old_l3 & ATTR_SW_WIRED) != 0)
