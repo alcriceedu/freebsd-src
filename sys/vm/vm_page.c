0909571276340c7e19316ebdf353788edb1b698c
Yufeng Zhou <yufeng.zhou@rice.edu>
2024-04-22 18:49:09 -0500

    Fix

--- sys/vm/vm_page.c
+++ sys/vm/vm_page.c
@@ -2821,7 +2821,7 @@ retry:
  *
  *	"req_class" must be an allocation class.
  */
-static int
+int
 vm_page_reclaim_run(int req_class, int domain, u_long npages, vm_page_t m_run,
     vm_paddr_t high)
 {
