1baa8c4520cc820d32c754582169a216a54b10f5
Alan Cox <alc@rice.edu>
2024-06-15 13:42:10 -0500

    simplify pmap_mincore

--- sys/arm64/arm64/pmap.c
+++ sys/arm64/arm64/pmap.c
@@ -8671,9 +8671,7 @@ pmap_mincore(pmap_t pmap, vm_offset_t addr, vm_paddr_t *pap)
 		}
 
 		managed = (tpte & ATTR_SW_MANAGED) != 0;
-		val = MINCORE_INCORE;
-		if (psind != 0)
-			val |= MINCORE_PSIND(psind);
+		val = MINCORE_INCORE | MINCORE_PSIND(psind);
 		if ((managed && pmap_pte_dirty(pmap, tpte)) || (!managed &&
 		    (tpte & ATTR_S1_AP_RW_BIT) == ATTR_S1_AP(ATTR_S1_AP_RW)))
 			val |= MINCORE_MODIFIED | MINCORE_MODIFIED_OTHER;
